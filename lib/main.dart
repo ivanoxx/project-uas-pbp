cff65fb3b92e0a28dd364140d0a58d2ed190c250
AndiRaihan <dzaky.raihan@gmail.com>
2022-11-28 21:28:24 +0700

    Added drawer

--- lib/main.dart
+++ lib/main.dart
@@ -74,6 +74,126 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [
+          Builder(
+            builder: (context) => IconButton(
+              icon: const CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"),
+              ),
+              onPressed: () => Scaffold.of(context).openEndDrawer(),
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Whistleblower',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32.0),
+              ),
+            ),
+            ListTile(
+              trailing: Icon(Icons.home),
+              title: Text('Homepage', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                // Here you can give your route to navigate
+              },
+            ),
+            ListTile(
+              trailing: Icon(Icons.account_balance),
+              title: Text('Hall of Shame', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                // Here you can give your route to navigate
+              },
+            ),
+            ListTile(
+              trailing: Icon(Icons.group_add),
+              title: Text('Create Forum', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                // Here you can give your route to navigate
+              },
+            ),
+            ListTile(
+              trailing: Icon(Icons.create),
+              title: Text('Create Post', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                // Here you can give your route to navigate
+              },
+            ),
+          ],
+        ),
+      ),
+      // Diambil dari https://blogmarch.com/flutter-left-right-navigation-drawer/
+      endDrawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.grey.shade500),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg'),
+                    radius: 40.0,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Text(
+                        'Andi Ayuna',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 25.0),
+                      ),
+                      SizedBox(height: 10.0),
+                      Text(
+                        'andi.ayuna@ui.ac.id',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 14.0),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+
+            //Here you place your menu items
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                // Here you can give your route to navigate
+              },
+            ),
+            const Divider(height: 3.0),
+            ListTile(
+              leading: Icon(Icons.my_library_books),
+              title: Text('My Post', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                // Here you can give your route to navigate
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Sign Out', style: TextStyle(fontSize: 18)),
+              onTap: () {
+                // Here you can give your route to navigate
+              },
+            ),
+          ],
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
