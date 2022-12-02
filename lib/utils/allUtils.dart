85baf074f9df779e0262bca859acc93cc6eb65d0
ivanoxx <dafinr13@gmail.com>
2022-12-02 23:49:33 +0700

    Finished edit alias

--- lib/utils/allUtils.dart
+++ lib/utils/allUtils.dart
@@ -1,5 +1,6 @@
 export 'FetchGroup.dart';
 export 'FetchMyPost.dart';
 export 'FetchTimeline.dart';
+export 'FetchProfile.dart';
 export 'package:pbp_django_auth/pbp_django_auth.dart';
 export 'package:provider/provider.dart';
