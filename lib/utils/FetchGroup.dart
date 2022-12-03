4647303e580edf23dc155d9b9219bd2dfe1919ba
AndiAyuna14 <andiayuna14@gmail.com>
2022-12-02 19:49:22 -0800

    landing page

--- lib/utils/FetchGroup.dart
+++ lib/utils/FetchGroup.dart
@@ -8,8 +8,8 @@ Future<List<Forum>> fetchGroup(request) async {
   // TODO: Ganti ke url railway
 
 
-  var response2 = await request.get("http://127.0.0.1:8000/show-group-json/");
-
+  //var response2 = await request.get("http://127.0.0.1:8000/show-group-json/");
+  var response2 = await request.get("https://whistle-blower.up.railway.app/show-group-json/");
 
   var data2 = response2;
 // melakukan konversi data json menjadi object ToDo
